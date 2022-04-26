222169159918ba71693400834506d0d1bef460b4
Akash Andotra <akashandotra456@gmail.com>
2022-04-26 20:13:58 +0530

    updated comment

--- api/app.ts
+++ api/app.ts
@@ -32,49 +32,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(` app listening on port ${port}`);
 });
-// import { Response } from "express";
-// require("dotenv").config();
-// const express = require("express");
-// const cookieParser = require("cookie-parser");
-// const { v4: uuidv4 } = require("uuid");
 
-// const sessions = require("express-session");
-
-// const router = require("./router.route");
-
-// const xss = require("xss-clean");
-// const path = require("path");
-// const cors = require("cors");
-
-// const app = express();
-
-// // const oneDay = 1000 * 60 * 60 * 24;
-
-// app.use(cors);
-// app.use(express.json());
-// app.use(cookieParser());
-
-// // app.use("/api", router);
-
-// // app.post("/api/park", (req, res) => {
-// //   console.log("abcdefgh");
-// //   res.json({
-// //     res: "res",
-// //   });
-// // });
-
-// app.get("/test", (req, res) => {
-//   console.log("TEST GOT");
-// });
-
-// app.get("/", (req, res) => {
-//   console.log("Home");
-// });
-
-// // app.use(express.static(path.join(__dirname, "Client")));
-
-// app.listen(5000, () => {
-//   console.log("Running at port " + process.env.APP_PORT);
-// });
